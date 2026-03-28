de2f922d8f9b5ade489f821a7b2fef3623712bde
swathishenoy25 <56560701+swathishenoy25@users.noreply.github.com>
2026-03-28 13:01:08 +0530

    Adding Titanic dataset exploration script
    Exploring Titanic dataset using pandas

---
+++ notebooks/day1_titanic_exploration.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+#Import the titanic dataset
+url="https://raw.githubusercontent.com/datasciencedojo/datsets/master/titanic.csv
+df= pd.read_csv(url)
+
+#Show first 5 rows
+print("First 5 rows are:")
+print(df.head())
+
+#Show basic information
+print("\nDataset info:")
+print(df.info())
+
+#Show missing values
+print("\nMissing values:")
+print(df.isnull().sum())
+
+#Show summary statistics
+print("\nSummary Statistics:")
+print(df.describe())
